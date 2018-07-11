a3dba1dcd238f05259ac18753992cdc4bd54a60e
qauto <bernat.bautista@axa-groupsolutions.com>
2018-07-11 14:35:39 +0200

    test5

---
+++ test5.js
@@ -0,0 +1,19 @@
+import { Builder, By, logging, until } from 'selenium-webdriver';
+
+describe("try", function() {
+    it("try", function() {
+        logging.installConsoleHandler();
+        logging.getLogger('promise.ControlFlow').setLevel(logging.Level.ALL);
+
+        let driver = new Builder()
+            .forBrowser('chrome')
+            .usingServer('http://selenium-hub:4444/wd/hub')
+            .build();
+
+        driver.get('http://www.google.com/ncr');
+        driver.findElement(By.name('q')).sendKeys('webdriver');
+        driver.findElement(By.name('btnK')).click();
+        driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+        driver.quit();
+    })
+})
