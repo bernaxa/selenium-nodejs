81dd8701cf563fab31257ff782c495e39e187e19
qauto <bernat.bautista@axa-groupsolutions.com>
2018-07-11 15:44:38 +0200

    update test5

--- test5.js
+++ test5.js
@@ -4,9 +4,14 @@ var logging = require('selenium-webdriver').logging;
 var prefs = new logging.Preferences();
     prefs.setLevel(logging.Type.DRIVER, logging.Level.ALL);
 
+var chromeCapabilities = webdriver.Capabilities.chrome();
+    chromeCapabilities.set('chromeOptions', {
+      'args': ['--headless', '--disable-gpu']
+    });
+
 var driver = new webdriver.Builder().
     usingServer('http://selenium-hub:4444/wd/hub').
-    withCapabilities(webdriver.Capabilities.chrome()).
+    withCapabilities(chromeCapabilities).
     setLoggingPrefs(prefs).
     build();
 
