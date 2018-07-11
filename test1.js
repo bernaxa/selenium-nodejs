0717c967986d8c0e03ff7c36db9f33cd84a891c1
qauto <bernat.bautista@axa-groupsolutions.com>
2018-07-11 11:38:46 +0200

    adding package and test1

---
+++ test1.js
@@ -0,0 +1,28 @@
+// Load dependecies
+var assert = require('chai').assert,
+    test = require('selenium-webdriver/testing'),
+    webdriver = require('selenium-webdriver');
+
+// Our test
+test.describe('Google', function () {
+    test.it('Title should be "Google"', function () {
+        // Set timeout to 10 seconds
+        this.timeout(10000);
+
+        // Get driver
+        var driver = new webdriver.Builder().
+        withCapabilities(webdriver.Capabilities.chrome()).
+        build();
+
+        // Go to URL
+        driver.get('http://www.google.com');
+
+        // Find title and assert
+        driver.executeScript('return document.title').then(function(return_value) {
+            assert.equal(return_value, 'Google')
+        });
+
+        // Quit webdriver
+        driver.quit();
+    });
+});
