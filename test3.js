defa055a0ca8f0e81efcd6242ffb285cf15abe5b
qauto <bernat.bautista@axa-groupsolutions.com>
2018-07-11 12:26:15 +0200

    adding test3

--- test3.js
+++ test3.js
@@ -1,18 +1,25 @@
-import webdriver from 'selenium-webdriver'
-import test from 'selenium-webdriver/testing'
 
-test.describe('Test', () => {
-  test.it('with headless chrome.', () => {
+var assert = require('assert'),
+var test = require('selenium-webdriver/testing'),
+var webdriver = require('selenium-webdriver');
+ 
+test.describe('Google Search', function() {
+  test.it('should work', function() {
     const chromeCapabilities = webdriver.Capabilities.chrome();
     chromeCapabilities.set('chromeOptions', {
       'args': ['--headless', '--disable-gpu']
     });
-    driver = new webdriver.Builder()
-      .usingServer('http://selenium-hub:4444/wd/hub')
-      .withCapabilities(chromeCapabilities)
-      .build();
+    var driver = new webdriver.Builder().
+    usingServer('http://selenium-hub:4444/wd/hub').
+    withCapabilities(chromeCapabilities).
+    build();
 
-    driver.get('http://www.google.com/');
+    driver.get('http://www.google.com');
+    var searchBox = driver.findElement(webdriver.By.name('q'));
+    searchBox.sendKeys('simple programmer');
+    searchBox.getAttribute('value').then(function(value) {
+      assert.equal(value, 'simple programmer');
+    });
     driver.quit();
-  })
-})
+  });
+});
