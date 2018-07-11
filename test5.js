ee3083944fdae2d52fa3ae60f4d84c0639dd035a
qauto <bernat.bautista@axa-groupsolutions.com>
2018-07-11 15:03:05 +0200

    test5

--- test5.js
+++ test5.js
@@ -1,8 +1,13 @@
 var webdriver = require('selenium-webdriver');
+var logging = require('selenium-webdriver').logging;
+
+var prefs = new logging.Preferences();
+    prefs.setLevel(logging.Type.BROWSER, logging.Level.ALL);
 
 var driver = new webdriver.Builder().
     usingServer('http://selenium-hub:4444/wd/hub').
     withCapabilities(webdriver.Capabilities.chrome()).
+    setLoggingPrefs(prefs).
     build();
 
 driver.get('http://www.google.com/ncr');
