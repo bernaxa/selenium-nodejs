cf8ed26c9313d8ac544b72fb15f6bf9352292596
Bernat Bautista <bernat.bautista@axa-groupsolutions.com>
2018-07-18 15:29:40 +0200

    Update TestClass.java

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
