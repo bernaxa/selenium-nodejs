1d0d26740894788a1a601a97b7fedca03fef7695
qauto <bernat.bautista@axa-groupsolutions.com>
2018-07-25 09:16:52 +0200

    adding cucumber

--- src/test/java/OpenMRSTests.java
+++ src/test/java/OpenMRSTests.java
@@ -1,5 +1,9 @@
 package org.cucumber.tests;
+
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
