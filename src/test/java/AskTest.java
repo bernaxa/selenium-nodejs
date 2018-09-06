879ce09b25f511a6b9f8f931d1b50f6d508bf54a
Bernat Bautista <bernat.bautista@axa-groupsolutions.com>
2018-09-06 10:26:56 +0200

    Update AskTest.java

--- src/test/java/AskTest.java
+++ src/test/java/AskTest.java
@@ -20,22 +20,52 @@ public class AskTest {
     @Test
     public void shouldAskGoogleForWebDriver() {
          
-        ChromeOptions opts = new ChromeOptions();
-        //driver = new RemoteWebDriver(new URL("http://selenium-node-chrome:5555/wd/hub"), opts);
+	Date now = new Date();
+	DateFormat formatter = DateFormat.getTimeInstance();        // time only
+	System.out.println("1: " + formatter.format(now));    
+	    
+        ChromeOptions options = new ChromeOptions();
+	options.add_argument('--headless')
+	options.add_argument("--no-sandbox")
+	options.add_argument("--disable-gpu")
+        //driver = new RemoteWebDriver(new URL("http://selenium-node-chrome:5555/wd/hub"), options);
 	
 	try {
-		//driver = new RemoteWebDriver(new URL("http://selenium-node-chrome:5555/wd/hub"), opts);
-		driver = new RemoteWebDriver(new URL("http://selenium-hub:4444/wd/hub"), opts);
-		//driver = new RemoteWebDriver(new URL("http://mobile-sp:f0467e7b-4024-4198-94f6-328384cf56a2@ondemand.saucelabs.com:80/wd/hub"), opts);
+		//driver = new RemoteWebDriver(new URL("http://selenium-node-chrome:5555/wd/hub"), options);
+		driver = new RemoteWebDriver(new URL("http://selenium-hub:4444/wd/hub"), options);
+		//driver = new RemoteWebDriver(new URL("http://mobile-sp:f0467e7b-4024-4198-94f6-328384cf56a2@ondemand.saucelabs.com:80/wd/hub"), options);
 	}
 	catch(MalformedURLException e) {
 		System.out.println("The url is not well formed: ");
 	}	    
 
+	now = new Date();
+	formatter = DateFormat.getTimeInstance();        // time only
+	System.out.println("2: " + formatter.format(now));    
+
 	driver.get("http://www.axa-im.com");
+
+	now = new Date();
+	formatter = DateFormat.getTimeInstance();        // time only
+	System.out.println("3: " + formatter.format(now));    
+
         System.out.println("Title 1: " + driver.getTitle());
+
+	now = new Date();
+	formatter = DateFormat.getTimeInstance();        // time only
+	System.out.println("4: " + formatter.format(now));    
+	    
         driver.get("https://demo.openmrs.org/openmrs/login.htm");
-        System.out.println("Title 1: " + driver.getTitle());
+	    
+	now = new Date();
+	formatter = DateFormat.getTimeInstance();        // time only
+	System.out.println("5: " + formatter.format(now));    
+
+        System.out.println("Title 2: " + driver.getTitle());
+
+	now = new Date();
+	formatter = DateFormat.getTimeInstance();        // time only
+	System.out.println("6: " + formatter.format(now));    
 
 	/*WebDriver browser = new FirefoxDriver();
 
