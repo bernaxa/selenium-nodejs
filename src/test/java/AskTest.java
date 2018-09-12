51a03f4aee909e16a41ceb329ad0f103b548ac09
Bernat Bautista <bernat.bautista@axa-groupsolutions.com>
2018-09-12 15:41:55 +0200

    Update AskTest.java

--- src/test/java/AskTest.java
+++ src/test/java/AskTest.java
@@ -56,8 +56,8 @@ public class AskTest {
 	    
 	try {
 		//driver = new RemoteWebDriver(new URL("http://celenium-node-chrome:5555/wd/hub"), options);
-		//driver = new RemoteWebDriver(new URL("http://selenium-node-chrome:5555/wd/hub"), options);
-		driver = new RemoteWebDriver(new URL("http://selenium-hub:4444/wd/hub"), options);
+		driver = new RemoteWebDriver(new URL("http://selenium-node-chrome:5555/wd/hub"), options);
+		//driver = new RemoteWebDriver(new URL("http://selenium-hub:4444/wd/hub"), options);
 		//driver = new RemoteWebDriver(new URL("http://mobile-sp:f0467e7b-4024-4198-94f6-328384cf56a2@ondemand.saucelabs.com:80/wd/hub"), options);
 		//driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), firefoxCapabilities);
 		//driver = new RemoteWebDriver(new URL("http://selenium-hub:4444/wd/hub"), options.toCapabilities());
